e63dcf3edd6906e89b1a9c3a67ac67177a185d5b
Sebastian Peter <s.peter@tum.de>
2025-03-11 00:04:33 +0100

    Initial commit

---
+++ src/main/java/org/ioc/container/IocContainer.java
@@ -0,0 +1,213 @@
+package org.ioc.container;
+
+import javax.inject.*;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.function.Supplier;
+
+public class IocContainer {
+
+  private final ConcurrentMap<Class<?>, Supplier<?>> singletons = new ConcurrentHashMap<>();
+  private final ConcurrentMap<Class<?>, Class<?>> contracts = new ConcurrentHashMap<>();
+  private final ConcurrentMap<String, Object> nameBindings = new ConcurrentHashMap<>();
+  private final ConcurrentMap<Class<? extends Annotation>, Object> qualifierBindings =
+      new ConcurrentHashMap<>();
+
+  public <T> T resolve(Class<T> type) {
+    return resolveInternal(type, new HashSet<>());
+  }
+
+  public <T> void registerSingleton(T instance) {
+    singletons.put(instance.getClass(), () -> instance);
+  }
+
+  public <T, V extends T> void registerSingleton(Class<T> contract, V instance) {
+    singletons.put(contract, () -> instance);
+  }
+
+  public <T> void registerContract(Class<T> contract) {
+    contracts.put(contract, contract);
+  }
+
+  public <T, V extends T> void registerContract(Class<T> contract, Class<V> binding) {
+    contracts.put(contract, binding);
+  }
+
+  public void registerNamed(String name, Object value) {
+    nameBindings.put(name, value);
+  }
+
+  public void registerQualified(Class<? extends Annotation> qualifier, final Object binding) {
+    qualifierBindings.put(qualifier, binding);
+  }
+
+  @SuppressWarnings("unchecked")
+  private <T> T resolveInternal(final Class<T> type, final Set<Class<?>> openTypeRequests) {
+    if (singletons.containsKey(type)) {
+      return type.cast(singletons.get(type).get());
+    }
+    if (openTypeRequests.contains(type)) {
+      throw new IllegalStateException("Cyclic dependency detected!");
+    }
+    openTypeRequests.add(type);
+    Class<T> requestedType;
+    if (contracts.containsKey(type)) {
+      requestedType = (Class<T>) contracts.get(type);
+    } else {
+      final Optional<Constructor<?>> defaultConstructor =
+          getDefaultConstructor(type.getConstructors());
+      if (!defaultConstructor.isPresent()) {
+        throw new IllegalStateException("Unknown type: " + type);
+      } else {
+        return callConstructor(defaultConstructor.get(), type);
+      }
+    }
+    final Constructor<?> injectionPoint = getInjectionPoint(requestedType.getConstructors());
+    final T resolvedValue =
+        callConstructor(
+            injectionPoint,
+            requestedType,
+            resolveDependencies(injectionPoint.getParameters(), openTypeRequests));
+    if (type.isAnnotationPresent(Singleton.class)) {
+      singletons.put(type, () -> resolvedValue);
+    }
+    return resolvedValue;
+  }
+
+  private Object[] resolveDependencies(
+      final Parameter[] parameters, Set<Class<?>> openTypeRequests) {
+    return Arrays.stream(parameters)
+        .map(
+            parameter -> {
+              final Class<?> type = parameter.getType();
+              if (parameter.isAnnotationPresent(Named.class)) {
+                return resolveNamed(parameter, openTypeRequests);
+              }
+              final Optional<Annotation> qualifier =
+                  Arrays.stream(parameter.getDeclaredAnnotations())
+                      .filter(
+                          annotation ->
+                              Arrays.stream(annotation.annotationType().getDeclaredAnnotations())
+                                  .anyMatch(
+                                      parentAnnotation ->
+                                          Qualifier.class.isAssignableFrom(
+                                              parentAnnotation.getClass())))
+                      .findFirst();
+              if (qualifier.isPresent()) {
+                return resolveQualifier(parameter, qualifier.get(), openTypeRequests);
+              }
+              if (type.equals(Provider.class)) {
+                return resolveProvider(parameter, openTypeRequests);
+              }
+              return resolveInternal(type, openTypeRequests);
+            })
+        .toArray();
+  }
+
+  @SuppressWarnings("unchecked")
+  private Object resolveQualifier(
+      final Parameter parameter, final Annotation qualifier, final Set<Class<?>> openTypeRequests) {
+    final Class<?> qualifierType = qualifier.annotationType();
+    final Object binding = qualifierBindings.get(qualifierType);
+    if (binding == null) {
+      throw new IllegalStateException("No type available for qualifier " + qualifierType);
+    }
+    final Object resolvedValue;
+    if (binding.getClass() != Class.class) {
+      resolvedValue = binding;
+    } else {
+      resolvedValue = resolveInternal((Class) binding, openTypeRequests);
+    }
+    final Class<?> requestedType = parameter.getType();
+    final Class<?> providedType = resolvedValue.getClass();
+    if (!requestedType.isAssignableFrom(resolvedValue.getClass())) {
+      throw new IllegalStateException(
+          "Incompatible type was provided! Requested type: "
+              + requestedType
+              + ", qualified by: "
+              + qualifierType
+              + ", provided type: "
+              + providedType);
+    }
+    return resolvedValue;
+  }
+
+  @SuppressWarnings("rawtypes")
+  private Provider<?> resolveProvider(
+      final Parameter parameter, final Set<Class<?>> openTypeRequests) {
+    if (parameter.getParameterizedType() instanceof ParameterizedType) {
+      final Class<?> requestedType =
+          (Class<?>)
+              ((ParameterizedType) parameter.getParameterizedType()).getActualTypeArguments()[0];
+      return (Provider) () -> resolve(requestedType);
+    }
+    return resolveInternal(Provider.class, openTypeRequests);
+  }
+
+  @SuppressWarnings({"unchecked", "rawtypes"})
+  private Object resolveNamed(final Parameter parameter, final Set<Class<?>> openTypeRequests) {
+    final String name = parameter.getAnnotation(Named.class).value();
+    if (nameBindings.get(name) == null) {
+      throw new IllegalStateException(
+          "Parameter @Named(\"" + name + "\") was requested but not provided!");
+    }
+    final Object value = nameBindings.get(name);
+    Object resolvedValue;
+    if (value instanceof Class) {
+      resolvedValue = resolveInternal((Class) value, openTypeRequests);
+    } else {
+      resolvedValue = value;
+    }
+    final Class<?> requestedType = parameter.getType();
+    final Class<?> providedType = resolvedValue.getClass();
+    if (!requestedType.isAssignableFrom(providedType)) {
+      throw new IllegalStateException(
+          "Incompatible type was provided! Requested type: "
+              + requestedType
+              + ", named: "
+              + name
+              + ", provided type: "
+              + providedType);
+    }
+    return resolvedValue;
+  }
+
+  private Constructor<?> getInjectionPoint(final Constructor<?>[] constructors) {
+    Constructor<?> defaultConstructor = null;
+    for (final Constructor<?> constructor : constructors) {
+      if (constructor.getAnnotation(Inject.class) != null) {
+        return constructor;
+      } else if (isDefaultConstructor(constructor)) {
+        defaultConstructor = constructor;
+      }
+    }
+    if (defaultConstructor != null) {
+      return defaultConstructor;
+    }
+    throw new IllegalStateException("No suitable constructor found!");
+  }
+
+  private boolean isDefaultConstructor(final Constructor<?> constructor) {
+    return constructor.getParameterTypes().length == 0
+        && Modifier.isPublic(constructor.getModifiers());
+  }
+
+  private Optional<Constructor<?>> getDefaultConstructor(final Constructor<?>[] constructors) {
+    return Arrays.stream(constructors).filter(this::isDefaultConstructor).findFirst();
+  }
+
+  private <T> T callConstructor(
+      final Constructor<?> constructor, final Class<T> type, final Object... params) {
+    try {
+      return type.cast(constructor.newInstance(params));
+    } catch (InstantiationException | InvocationTargetException | IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
---
+++ src/test/java/org/ioc/container/IocContainerTest.java
@@ -0,0 +1,220 @@
+package org.ioc.container;
+
+import org.junit.jupiter.api.Test;
+
+import javax.inject.*;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IocContainerTest {
+
+  public static class ServiceB {
+
+    public ServiceB(final String someString) {}
+  }
+
+  public static class ServiceC {
+
+    @Inject
+    public ServiceC(final ServiceA serviceA) {}
+  }
+
+  public static class ServiceD {
+
+    @Inject
+    public ServiceD(final ServiceE serviceE) {}
+  }
+
+  public static class ServiceE {
+
+    @Inject
+    public ServiceE(final ServiceD serviceD) {}
+  }
+
+  public static class ServiceF {
+
+    private final Provider<ServiceB> serviceBProvider;
+
+    @Inject
+    public ServiceF(final Provider<ServiceB> serviceB) {
+      this.serviceBProvider = serviceB;
+    }
+
+    public Provider<ServiceB> getServiceBProvider() {
+      return serviceBProvider;
+    }
+  }
+
+  public static class ServiceG {
+
+    private final Provider<ServiceH> provider;
+
+    @Inject
+    public ServiceG(final Provider<ServiceH> provider) {
+      this.provider = provider;
+    }
+  }
+
+  public static class ServiceH {
+
+    @Inject
+    public ServiceH(final ServiceG serviceG) {}
+  }
+
+  public static class ServiceI {
+
+    private final String something;
+
+    @Inject
+    public ServiceI(final @Named("something") String something) {
+      this.something = something;
+    }
+  }
+
+  public static class ServiceJ implements Contract {}
+
+  public static class ServiceK implements Contract {}
+
+  public static class ServiceL {
+
+    private Contract contract;
+
+    @Inject
+    public ServiceL(final @Named("requestServiceK") Contract contract) {
+      this.contract = contract;
+    }
+  }
+
+  @Singleton
+  public static class ServiceM {}
+
+  public static class ServiceN {
+
+    private final Contract contract;
+
+    @Inject
+    public ServiceN(final @SpecialType Contract contract) {
+      this.contract = contract;
+    }
+  }
+
+  public interface Contract {}
+
+  @Qualifier
+  @Retention(RetentionPolicy.RUNTIME)
+  public @interface SpecialType {}
+
+  private final IocContainer container = new IocContainer();
+
+  public static class ServiceA implements Contract {}
+
+  @Test
+  public void shouldResolveServiceWithoutDependencies() {
+    assertNotNull(container.resolve(ServiceA.class));
+  }
+
+  @Test
+  public void shouldThrowOnMissingSuitableConstructor() {
+    assertThrows(IllegalStateException.class, () -> container.resolve(ServiceB.class));
+  }
+
+  @Test
+  public void shouldResolveProvided() {
+    final ServiceB serviceB = new ServiceB("something");
+    container.registerSingleton(ServiceB.class, serviceB);
+    assertSame(serviceB, container.resolve(ServiceB.class));
+  }
+
+  @Test
+  public void shouldResolveByContract() {
+    container.registerContract(Contract.class, ServiceA.class);
+    assertInstanceOf(ServiceA.class, container.resolve(Contract.class));
+  }
+
+  @Test
+  public void shouldResolveDependencies() {
+    container.registerContract(ServiceC.class);
+    assertNotNull(container.resolve(ServiceC.class));
+  }
+
+  @Test
+  public void shouldRequireContractRegistration() {
+    assertThrows(IllegalStateException.class, () -> container.resolve(ServiceC.class));
+  }
+
+  @Test
+  public void shouldHandleCyclicDependencies() {
+    container.registerContract(ServiceE.class);
+    assertThrows(IllegalStateException.class, () -> container.resolve(ServiceD.class));
+  }
+
+  @Test
+  public void shouldInjectProviders() {
+    final ServiceB serviceB = new ServiceB("something");
+    container.registerSingleton(serviceB);
+    container.registerContract(ServiceF.class);
+    assertNotNull(container.resolve(ServiceF.class));
+    assertSame(serviceB, container.resolve(ServiceF.class).getServiceBProvider().get());
+  }
+
+  @Test
+  public void shouldInjectLazyProviders() {
+    container.registerContract(ServiceH.class);
+    container.registerContract(ServiceG.class);
+    final ServiceG serviceG = container.resolve(ServiceG.class);
+    assertNotNull(serviceG);
+    assertNotNull(container.resolve(ServiceH.class));
+    assertNotNull(serviceG.provider.get());
+  }
+
+  @Test
+  public void shouldProvideNamed() {
+    container.registerNamed("something", "my provided thing");
+    container.registerContract(ServiceI.class);
+    assertEquals("my provided thing", container.resolve(ServiceI.class).something);
+  }
+
+  @Test
+  public void shouldThrowOnWrongNamedType() {
+    container.registerNamed("something", new Object());
+    container.registerContract(ServiceI.class);
+    assertThrows(IllegalStateException.class, () -> container.resolve(ServiceI.class));
+  }
+
+  @Test
+  public void shouldThrowOnWrongQualifiedType() {
+    container.registerQualified(SpecialType.class, new Object());
+    container.registerContract(ServiceN.class);
+    assertThrows(IllegalStateException.class, () -> container.resolve(ServiceN.class));
+  }
+
+  @Test
+  public void shouldPrioritizeNamedRegistrationOverContractRegistration() {
+    container.registerContract(Contract.class, ServiceJ.class);
+    container.registerNamed("requestServiceK", ServiceK.class);
+    container.registerContract(ServiceL.class);
+    assertInstanceOf(ServiceK.class, container.resolve(ServiceL.class).contract);
+  }
+
+  @Test
+  public void shouldPrioritizeQualifierRegistrationOverContractRegistration() {
+    container.registerContract(Contract.class, ServiceJ.class);
+    container.registerQualified(SpecialType.class, ServiceK.class);
+    container.registerContract(ServiceN.class);
+    assertInstanceOf(ServiceK.class, container.resolve(ServiceN.class).contract);
+  }
+
+  @Test
+  public void shouldReuseSingletonInstances() {
+    container.registerContract(ServiceM.class);
+    assertSame(container.resolve(ServiceM.class), container.resolve(ServiceM.class));
+  }
+
+  @Test
+  public void shouldProvideNewInstancesForNonSingleton() {
+    container.registerContract(ServiceA.class);
+    assertNotSame(container.resolve(ServiceA.class), container.resolve(ServiceA.class));
+  }
+}
